8767eb40484701ace44b1e8f4882045ebf0e8ce3
Lefie <qiwenruan11@gmail.com>
2025-04-25 16:29:55 -0400

    set up a table of content for compoent libraries

--- main.jsx
+++ main.jsx
@@ -1,9 +1,19 @@
 import { createRoot } from 'react-dom/client'
 
 function App() {
-    return (<>
-    <h1>Hello World</h1>
-    </>)
+
+    return (
+    <>
+        <div className='components-container'>
+            <h1>Components List</h1>
+            <div className="components-list">
+                <a href="#"><div className='item'>Badges</div></a>
+                <a href="#"><div className='item'>Banner</div></a>
+                <a href="#"><div className='item'>Cards</div></a>
+            </div>
+        </div>
+    </>
+    )
 }
 
 createRoot(document.getElementById('root')).render(
